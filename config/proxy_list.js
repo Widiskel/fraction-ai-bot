74f92d91f093ec515cc2cd1459a818dab5e56c3f
Widianto Eka saputro <97203329+Widiskel@users.noreply.github.com>
2025-01-22 23:00:36 +0700

    Delete config/proxy_list.js

--- config/proxy_list.js
+++
@@ -1,7 +0,0 @@
-export const proxyList = [
-  "http://wtzdtsyve6mtpl5-session-d06pc80dq1-lifetime-30:y4ir15bia5jgm8p@51.159.85.23:6060",
-  "http://wtzdtsyve6mtpl5-session-d06pc80dq1-lifetime-30:y4ir15bia5jgm8p@51.159.85.23:6060",
-  "http://wtzdtsyve6mtpl5-session-d06pc80dq1-lifetime-30:y4ir15bia5jgm8p@51.159.85.23:6060",
-  // "http://bmvsyqqq:kt78sojwim80@198.23.239.134:6540",
-  //   "http://bmvsyqqq:kt78sojwim80@167.160.180.203:6754",
-];
